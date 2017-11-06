35978f8efe62aa58dac8edfb7dd19b49f7446817
Max Körlinge <mkorlinge@hotmail.com>
2017-11-06 15:04:44 +0100

    login page html and css created

---
+++ login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Tasty Recipes</title>
+        <link rel="stylesheet" href="css/normalize.css"> <!-- used as reset -->
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <header>
+            <?php include 'parts/header.php' ?>
+        </header>
+
+<div class="login-page">
+ <form action="/action_page.php">
+
+  <div class="container">
+    <label><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button class "w3cbutton" type="submit">Login</button>
+    <input type="checkbox" checked="checked"> Remember me
+  </div>
+
+  <div class="container" style="background-color:#b3b3ff">
+    <button type="button" class="w3cbutton cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form> 
+
+</div>
+        <footer>
+            <?php include 'parts/footer.php' ?>
+        </footer>
+    </body>
+</html>
