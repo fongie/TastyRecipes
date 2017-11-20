2f748cc19d0cab399085069090378ae092fe9ce1
Max Körlinge <mkorlinge@hotmail.com>
2017-11-20 09:23:22 +0100

    ready to start writing the classes

---
+++ src/controller/Controller.php
@@ -0,0 +1,33 @@
+<?php
+
+class Controller {
+    private $uaHandler; //user account handler
+
+    public function __construct() {
+
+    }
+
+    public function login($username, $password) {
+
+    }
+
+    public function register($username, $password) {
+
+    }
+
+    public function recipeSiteContent($recipeName) {
+
+    }
+    public function recipeSiteComments($recipeName) {
+
+    }
+    public function postComment($commentText) {
+
+    }
+
+    public function deleteComment($commentID) {
+
+    }
+
+}
+?>
