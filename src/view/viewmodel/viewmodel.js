331d97118416107ecd60d5d2bed2ed2a55788e20
Max Körlinge <mkorlinge@hotmail.com>
2017-11-27 21:29:41 +0100

    refr to using javascript classes

--- src/view/viewmodel/viewmodel.js
+++ src/view/viewmodel/viewmodel.js
@@ -1,26 +1,39 @@
-$(document).ready(function () {
+$(document).ready(() => {
 
-    function loginHeader() {
-        this.loggedIn = false;
-        this.notLoggedIn = ko.observable(this.loggedIn);
-        this.isLoggedIn = ko.observable(!(this.loggedIn));
-        this.userName = ko.observable();
-        this.text = ko.observable("Logged in as");
-        this.loggedinText = ko.computed(
-            () => (this.text() + " " + this.userName()),
-            this
-        );
+    //Controls the login header part
+    class loginHeader {
+        constructor() {
+            this.getInformationFromServer = this.getInformationFromServer.bind(this);
 
-        $.getJSON(
-            "/src/view/requests/getLogin.php", 
-            (resp) => { 
-                this.userName(resp.username); 
-                this.notLoggedIn(resp.loggedIn);
-                this.isLoggedIn(!resp.loggedIn);
-            }
-        );
+            //Class variables
+            this.loggedIn = false;
+            this.notLoggedIn = ko.observable(this.loggedIn);
+            this.isLoggedIn = ko.observable(!(this.loggedIn));
+            this.userName = ko.observable();
+            this.text = ko.observable("Logged in as");
+
+            this.loggedinText = ko.computed(
+                () => (this.text() + " " + this.userName()),
+                this
+            );
+
+            //Ajax call
+            this.getInformationFromServer();
+
+        }
+
+        getInformationFromServer() {
+            $.getJSON(
+                "/src/view/requests/getLogin.php", 
+                (resp) => { 
+                    this.userName(resp.username); 
+                    this.notLoggedIn(resp.loggedIn);
+                    this.isLoggedIn(!resp.loggedIn);
+                }
+            );
+        }
     }
-    
-ko.applyBindings(new loginHeader(), document.getElementById('login-div'));
+
+    ko.applyBindings(new loginHeader(), document.getElementById('login-div'));
 
 });
