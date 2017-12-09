26cccfb7120adf0ecdafc726eceaddca030e6ee6
Max Körlinge <mkorlinge@hotmail.com>
2017-12-09 15:51:06 +0100

    working getting comments and displaying them via viewmodel

--- src/view/parts/comments.php
+++ src/view/parts/comments.php
@@ -4,14 +4,14 @@ session_start();
 echo '
             <div class="usercomments">
                 <h4>Comments:</h4>
-                <ul id="comments-list" data-bind="foreach: comment">
+                <ul id="comments-list" data-bind="foreach: comments">
                     <li>
                         <div class="comment">
                             <div class="username-div">
-                                <p><strong data-bind="text: commentUser"></strong></p>
+                                <p><strong data-bind="text: username"></strong></p>
                             </div>
                             <div class="comment-div">
-                            <p data-bind="text: commentText"></p>
+                            <p data-bind="text: comment"></p>
                             </div>
                         </div>
                     </li>
--- src/view/viewmodel/commentSection.js
+++ src/view/viewmodel/commentSection.js
@@ -1,33 +1,12 @@
 $(document).ready(() => {
     class commentSection {
         constructor() {
-            this.fetchComments = this.fetchComments.bind(this);
-            const comments = this.fetchComments();
-            this.comment = ko.observableArray(comments);
-
-        }
-
-        fetchComments() {
+            this.comments = ko.observableArray();
             $.getJSON(
                 "/src/view/requests/get_comments.php", 
-                (resp) => { 
-                    console.log(resp);
-                }
+                (resp) => (this.comments(resp))
             );
-            let comments = [
-                { 
-                    commentUser: 'User',
-                    commentText: 'Hello guys'
-                },
-                {
-                    commentUser: 'Foo',
-                    commentText: 'Bar'
-                }
-            ];
-
-            return comments;
         }
     }
-
     ko.applyBindings(new commentSection(), document.getElementById('comments-list'));
 });
